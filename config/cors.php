9fac7eaa663b6c27fb43dc38e84ef1542e7caa36
Marly Rangel <129441027+Mrangel793@users.noreply.github.com>
2026-09-06 11:54:10 -0500

    Mejora: Actualizar configuraciones de CORS para incluir dominios permitidos

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,17 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177'],
+    'allowed_origins' => [
+        'https://hiloblanco.co',
+        'https://www.hiloblanco.co',
+        'http://hiloblanco.co',
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'http://localhost:5176',
+        'http://localhost:5177',
+    ],
 
     'allowed_origins_patterns' => [],
 
